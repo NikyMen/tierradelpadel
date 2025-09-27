60c6c10f133001166cec17add496b4cf2b861170
nico <niko.moto@hotmail.com.ar>
2025-09-27 16:00:55 -0300

    fix error deploy

--- .vercel/output/functions/render.func/dist/entry.mjs
+++ .vercel/output/functions/render.func/dist/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_vmUndfwH.mjs';
-import { manifest } from './manifest_BZVacQx2.mjs';
+import { manifest } from './manifest_DBQ5NpDw.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/admin.astro.mjs');
